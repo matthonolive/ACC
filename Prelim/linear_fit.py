e5be156e6bf9fd79c9b7c870acefa433ae7b0bff
Matthew Oliveira <matthewhdmd@gmail.com>
2026-03-29 01:00:49 -0400

    linear_fit.py

--- Prelim/linear_fit.py
+++ Prelim/linear_fit.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+'''
+Generic linea rcurve fit script
+'''
+
+
 
 # -----------------------------
 # User settings
